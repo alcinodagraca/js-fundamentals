b74cfb823bcfec246f9639685e64f881ae786f9f
Alcino da Graça <alcinodagrsca@gmail.com>
2019-07-31 17:19:54 +0200

    Estudei a Destruting Features

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,11 @@
-import '../styles/index.scss';
+const carId = 100;
 
-console.log('webpack starterkit');
+//carId = 5;
+
+//console.log(carId);
+
+// function sendCars(day, ...carId){
+//     carId.forEach( id => console.log(id) );
+// }
+
+// sendCars('Monday', 1, 2, 3);
